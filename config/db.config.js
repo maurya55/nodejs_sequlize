507b88654cb4652c43483394bc81dd0819cdea2b
Rahulmaurya55 <rahul.tortalabz@gmail.com>
2022-06-30 21:43:32 +0530

    change database psw

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   'sequlize',
   'root',
-  '', {
+  '123456', {
 
       // Explicitly specifying 
       // mysql database
